87ba6ae90a9ac7ef3d5fa6090695db7628073736
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-05-23 21:01:57 +0530

    Add files via upload

--- CheckboxExample.js
+++ CheckboxExample.js
@@ -3,40 +3,70 @@ import { useState } from "react"
 var CheckboxExample =()=>{
     var[c1,setC1] = useState(undefined)
     var[c2,setC2] = useState(undefined)
+    var[c3,setC3] = useState(undefined)
+    var[c4,setC4] = useState(undefined)
+    var[show1,setShow1] = useState("block")
+    var[show2,setShow2] = useState("none")
+    var[show3,setShow3] = useState("none")
     var[res,setRes] = useState(undefined)
     var displayCourse = (e)=>{
            var data= ""
-           var data1 = ""
-           if(c1==true)
+        //   var data1 = ""
+           if(c1 && c3)
            {
-            data = "Java"
+            data = "Correct Answer"
            }
            else
            {
-            data = ""
+            data = "Incorrect Answer"
            }
-           if(c2==true)
-           {
-            data1 = ".NET"
-           }
-           else
-           {
-            data1 = ""
-           }
+           setShow1("none")
+           setShow2("block")
            
-            setRes(data+ " " + data1)
+            setRes(data)
             e.preventDefault()
     }
     return(<div>
-         <form onSubmit={displayCourse} >
+        
+         <div id="set1" style={{display:show1}}>
           <label>Select Course</label>
           <br/>
           <input type="checkbox" onChange={(e)=>setC1(e.target.checked)} name="course" value="JAVA"  /> JAVA
           <br />
           <input type="checkbox" onChange={(e)=>setC2(e.target.checked)} name="course" value=".NET"  /> .NET
           <br />
-          <input type="submit" name="btnsubmit" value="Click" />
-          </form>
+          <input type="checkbox" onChange={(e)=>setC3(e.target.checked)} name="course" value="PHP"  /> JAVA
+          <br />
+          <input type="checkbox" onChange={(e)=>setC4(e.target.checked)} name="course" value="iOS"  /> iOS
+          <br />
+          <input type="button" name="btnsubmit" value="NEXT" onClick={displayCourse} />
+         </div>
+         <div id="set2" style={{display:show2}}>
+          <label>Select Course2</label>
+          <br/>
+          <input type="checkbox" onChange={(e)=>setC1(e.target.checked)} name="course" value="JAVA"  /> JAVA
+          <br />
+          <input type="checkbox" onChange={(e)=>setC2(e.target.checked)} name="course" value=".NET"  /> .NET
+          <br />
+          <input type="checkbox" onChange={(e)=>setC3(e.target.checked)} name="course" value="PHP"  /> JAVA
+          <br />
+          <input type="checkbox" onChange={(e)=>setC4(e.target.checked)} name="course" value="iOS"  /> iOS
+          <br />
+          <input type="button" name="btnsubmit" value="NEXT" onClick={displayCourse} />
+         </div>
+         <div id="set3" style={{display:show3}}>
+          <label>Select Course3</label>
+          <br/>
+          <input type="checkbox" onChange={(e)=>setC1(e.target.checked)} name="course" value="JAVA"  /> JAVA
+          <br />
+          <input type="checkbox" onChange={(e)=>setC2(e.target.checked)} name="course" value=".NET"  /> .NET
+          <br />
+          <input type="checkbox" onChange={(e)=>setC3(e.target.checked)} name="course" value="PHP"  /> JAVA
+          <br />
+          <input type="checkbox" onChange={(e)=>setC4(e.target.checked)} name="course" value="iOS"  /> iOS
+          <br />
+          <input type="button" name="btnsubmit" value="Finish" onClick={displayCourse} />
+         </div>
           {res}
     </div>)
 }
--- RadioExample.js
+++ RadioExample.js
@@ -3,17 +3,27 @@ import { useState } from "react"
 var RadioExample =()=>{
     var[c,setC] = useState(undefined)
     var[res,setRes] = useState("blue")
+    var[show,setShow] = useState("hidden");
     var displayCourse = (e)=>{
             setRes(c)
             e.preventDefault()
+    }
+    var otherColor = (e)=>{
+          setShow(true)
     }
     return(<div style={{backgroundColor:res,width:'500px',height:'500px'}}>
          <form onSubmit={displayCourse} >
           <label>Select Course</label>
           <br/>
-          <input type="radio" onChange={(e)=>setC(e.target.value)} name="course" value="RED"  /> RED
+          <input type="radio" onChange={(e)=>setC(e.target.value)} 
+          name="course" value="RED"  /> RED
           <br />
-          <input type="radio" onChange={(e)=>setC(e.target.value)} name="course" value="GREEN"  /> GREEN
+          <input type="radio" onChange={otherColor}
+           name="course" value="Green"  /> Other
+          <div  style={{visibility:show}}>
+            <input type="text" placeholder="Enter color" 
+            onChange={(e)=>setC(e.target.value)} />
+          </div>
           <br />
           <input type="submit" name="btnsubmit" value="Click" />
           </form>
